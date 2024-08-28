0b7b15a5e651c7b1b364726e582cc43c051d65eb
Jonathan <deroo.jon@gmail.com>
2024-08-28 11:53:16 +0200

    Admin login seems complete, now it needs to be run!

--- src/app/admin-register/admin-register.component.ts
+++ src/app/admin-register/admin-register.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin-register.component.html',
-  styleUrl: './admin-register.component.css'
+  styleUrls: ['./admin-register.component.css']
 })
 export class AdminRegisterComponent {
+  admin = {
+    username: '',
+    password: '',
+    confirmPassword: ''
+  };
 
+  passwordMisMatch = false;
+
+  constructor(private http: HttpClient) {}
+
+registerAdmin() {
+  if (this.admin.password !== this.admin.confirmPassword) {
+    this.passwordMisMatch = true;
+    return;
+  }
+
+  this.passwordMisMatch = false;
+
+
+
+  
+    this.http.post<any>('/api/admins/registerAdmin', this.admin).subscribe(
+      () => {
+        console.log('Admin registered successfully');
+        // Add any additional logic or redirect to a success page
+      },
+      (error) => {
+        console.error('Failed to register admin:', error);
+        // Handle the error, display an error message, or redirect to an error page
+      }
+    );
+  }
 }
--- src/app/doctor-register/doctor-register.component.ts
+++ src/app/doctor-register/doctor-register.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-doctor-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './doctor-register.component.html',
   styleUrl: './doctor-register.component.css'
 })
 export class DoctorRegisterComponent {
+  doctor = {
+    firstName: '',
+    lastName: '',
+    rizivNumber: '',
+    password: '',
+    confirmPassword: ''
+  };
 
+  constructor(private http: HttpClient) {}
+
+  registerDoctor() {
+    this.http.post('/api/doctors/registerDoctor', this.doctor).subscribe(
+      () => {
+        console.log('Doctor registered successfully');
+        // Add any additional logic or redirect to a success page
+      },
+      (error) => {
+        console.error('Failed to register doctor:', error);
+        // Handle the error, display an error message, or redirect to an error page
+      }
+    );
+    
+  }
 }
